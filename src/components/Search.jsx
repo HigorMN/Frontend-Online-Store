d031075c632aa6d4eb6dc99591f5157fedf9dbc2
DanielRohe86 <psicologo.rohe@gmail.com>
2022-09-08 19:58:48 -0300

    add req5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Search extends Component {
   state = {
     search: '',
+    cards: [],
+    // name: '',
+    // image: '',
+    // price: '',
   };
 
   onInputChange = ({ target }) => {
@@ -15,9 +20,16 @@ export default class Search extends Component {
 
   // };
 
-  render() {
+  handleClick = async () => {
     const { search } = this.state;
+    const products = await getProductsFromCategoryAndQuery(search);
+    this.setState({ cards: products.results });
+  };
+
+  render() {
+    const { search, cards } = this.state;
     const zero = 0;
+    const string = 'Nenhum produto foi encontrado';
 
     return (
       <div>
@@ -28,12 +40,38 @@ export default class Search extends Component {
             id=""
             onChange={ this.onInputChange }
             value={ search }
+            data-testid="query-input"
           />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.handleClick }
+          >
+            🔎
+          </button>
         </form>
         { search.length === zero && (
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>)}
+
+        { cards.length === zero
+          ? (<p>{string}</p>)
+          : (
+            <div
+              data-testid="product"
+            >
+              { cards.map((product) => (
+                <div
+                  data-testid="product"
+                  key={ product.id }
+                >
+                  <p>{product.title}</p>
+                  <p>{product.price}</p>
+                  <img src={ product.thumbnail } alt={ product.id } />
+                </div>))}
+            </div>)}
+
       </div>
     );
   }
