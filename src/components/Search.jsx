28b85ad121c05354f43c31c41bfda586f3c01323
Eduardo-Balieiro <eduferabalfer56@gmail.com>
2022-09-08 16:54:45 -0300

    req 2 metade

---
+++ src/components/Search.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  state = {
+    search: '',
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  };
+
+  // searchEmpty = () => {
+
+  // };
+
+  render() {
+    const { search } = this.state;
+    //   const zero = 0
+
+    return (
+      <div>
+          { search ?  <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p> };
+        <form>
+          <input
+            type="text"
+            name="search"
+            id=""
+            onChange={ this.onInputChange }
+            value={ search }
+          />
+        </form>
+      </div>
+    );
+  }
+}
