104c96466b15012e580b784c7a009ac12a041244
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-08 18:20:57 -0300

    Requisito 04 concluido

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 export default class Search extends Component {
   state = {
     search: '',
+    categoriesList: [],
   };
+
+  componentDidMount() {
+    this.fetchAPIgetCategories();
+  }
 
   onInputChange = ({ target }) => {
     const { name, type } = target;
@@ -11,30 +17,44 @@ export default class Search extends Component {
     this.setState({ [name]: value });
   };
 
-  // searchEmpty = () => {
-
-  // };
+  fetchAPIgetCategories = async () => {
+    const categories = await getCategories();
+    this.setState({ categoriesList: categories });
+  };
 
   render() {
-    const { search } = this.state;
+    const { search, categoriesList } = this.state;
     const zero = 0;
 
     return (
-      <div>
-        <form>
-          <input
-            type="text"
-            name="search"
-            id=""
-            onChange={ this.onInputChange }
-            value={ search }
-          />
-        </form>
-        { search.length === zero && (
-          <p data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>)}
-      </div>
+      <main className="main">
+        <section className="categories">
+          <p>Categorias</p>
+          {categoriesList.map((e) => (
+            <div key={ e.id }>
+              <label htmlFor={ e.id } data-testid="category">
+                <input type="radio" name={ e.name } id={ e.id } />
+                <span>{e.name}</span>
+              </label>
+            </div>
+          ))}
+        </section>
+        <div>
+          <form>
+            <input
+              type="text"
+              name="search"
+              id=""
+              onChange={ this.onInputChange }
+              value={ search }
+            />
+          </form>
+          { search.length === zero && (
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>)}
+        </div>
+      </main>
     );
   }
 }
