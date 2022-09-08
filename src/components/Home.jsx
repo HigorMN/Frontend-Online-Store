067347b764b995102c2d93ab032b292bdd5f07fc
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-08 20:18:09 -0300

    mudando nome do component para home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { getCategories } from '../services/api';
 
-export default class Search extends Component {
+export default class Home extends Component {
   state = {
     search: '',
     categoriesList: [],
