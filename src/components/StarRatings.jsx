9f98830be560304acf9374f21056d387b26f2f23
HigorMN <higor.maranhao2000@gmail.com>
2022-11-28 12:45:42 -0300

    feat: StarRating criado

--- src/components/StarRatings.jsx
+++ src/components/StarRatings.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
 
-import './StarRatings.css';
+const FIVE = 5;
+
+const colors = {
+  orange: '#FFBA5A',
+  grey: '#a9a9a9',
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  stars: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+};
 
 export default function StarRatings() {
+  const [currentValue, setCurrentValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(FIVE).fill(0);
+
+  const handleClick = (value) => {
+    setCurrentValue(value);
+  };
+
+  const handleMouseOver = (newHoverValue) => {
+    setHoverValue(newHoverValue);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverValue(undefined);
+  };
+
   return (
-    <div>StarRatings</div>
+    <div style={ styles.container }>
+      <div styles={ styles.stars }>
+        {stars.map((_, index) => (
+          <FaStar
+            key={ index }
+            size={ 24 }
+            onClick={ () => handleClick(index + 1) }
+            onMouseOver={ () => handleMouseOver(index + 1) }
+            onMouseLeave={ handleMouseLeave }
+            color={ (hoverValue || currentValue) > index ? colors.orange : colors.grey }
+            style={ {
+              marginRight: 5,
+              cursor: 'pointer',
+            } }
+          />
+        ))}
+      </div>
+    </div>
   );
 }
